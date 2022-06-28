b389b6c308ea6f70232cb370d492b61abb5466e7
luz <ruizhexueluzhuan@gmail.com>
2022-06-29 02:47:08 +0800

    debugging

--- environment.py
+++ environment.py
@@ -17,9 +17,12 @@ def EnvironmentStore(env):  # 保存环境
 
 
 def step(action, state):  # 按指令行动后反应，智能体与环境交互
-    state.position = state.position + action
-    GrassEnergy = 0.1 * state.egrass[state.position]
-    TerrainEnergy = state.energy * state.eterrian
+    state.position = np.add(state.position, action)
+    print(state.position)
+    GrassEnergy = 0.1 * state.egrass[state.position[0]][state.position[1]]
+    print(GrassEnergy)
+    TerrainEnergy = np.multiply(state.energy, state.eterrian)
+    print(TerrainEnergy)
     state.energy = state.energy + GrassEnergy - TerrainEnergy
     state.egrass[state.position] = state.egrass[state.position] * 0.9
     reward = GrassEnergy - TerrainEnergy
