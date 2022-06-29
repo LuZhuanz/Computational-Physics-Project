caa7e084b363bff7acce8f741685c83e57c3f06f
luz <ruizhexueluzhuan@gmail.com>
2022-06-29 17:04:57 +0800

    debugging change 99/98

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import StateD
 
 
 def choose_action(state_e):  # 由Q表格选择策略,羊群策略记入action
@@ -13,21 +14,24 @@ def choose_action(state_e):  # 由Q表格选择策略,羊群策略记入action
                 action = [1, 0]
             else:
                 action = [0, 1]
-        elif state_e.position[1] == 99:
-            if state_e.Qlearning[99][1] == 0 and state_e.Qlearning[98][0] == 0:
+        elif state_e.position[1] == StateD.constant.size:
+            if state_e.Qlearning[StateD.constant.size][1] == 0 and state_e.Qlearning[StateD.constant.size - 1][0] == 0:
                 split_1 = 0.5
             else:
-                split_1 = state_e.Qlearning[0][98] / (state_e.Qlearning[0][98] + state_e.Qlearning[1][99])
+                split_1 = state_e.Qlearning[0][StateD.constant.size - 1] / (
+                            state_e.Qlearning[0][StateD.constant.size - 1] + state_e.Qlearning[1][StateD.constant.size])
             seed = np.random.random()
             if split_1 < seed:
                 action = [1, 0]
             else:
                 action = [0, -1]
         else:
-            Qsum = state_e.Qlearning[0][int(state_e.position[1]) + 1] + state_e.Qlearning[0][int(state_e.position[1]) - 1] + \
+            Qsum = state_e.Qlearning[0][int(state_e.position[1]) + 1] + state_e.Qlearning[0][
+                int(state_e.position[1]) - 1] + \
                    state_e.Qlearning[1][int(state_e.position[1])]
             split_1 = state_e.Qlearning[0][int(state_e.position[1]) + 1] / Qsum
-            split_2 = (state_e.Qlearning[0][int(state_e.position[1]) + 1] + state_e.Qlearning[1][int(state_e.position[1])]) / Qsum
+            split_2 = (state_e.Qlearning[0][int(state_e.position[1]) + 1] + state_e.Qlearning[1][
+                int(state_e.position[1])]) / Qsum
             seed = np.random.random()
             if seed < split_1:
                 action = [0, 1]
@@ -35,37 +39,44 @@ def choose_action(state_e):  # 由Q表格选择策略,羊群策略记入action
                 action = [1, 0]
             else:
                 action = [0, 1]
-    elif state_e.position[0] == 99:
+    elif state_e.position[0] == StateD.constant.size:
         if state_e.position[1] == 0:
-            if state_e.Qlearning[99][1] == 0 and state_e.Qlearning[98][0] == 0:
+            if state_e.Qlearning[StateD.constant.size][1] == 0 and state_e.Qlearning[StateD.constant.size - 1][0] == 0:
                 split_1 = 0.5
             else:
-                split_1 = state_e.Qlearning[99][1] / (state_e.Qlearning[99][1] + state_e.Qlearning[98][0])
+                split_1 = state_e.Qlearning[StateD.constant.size][1] / (
+                            state_e.Qlearning[StateD.constant.size][1] + state_e.Qlearning[StateD.constant.size - 1][0])
             seed = np.random.random()
             if split_1 < seed:
                 action = [-1, 0]
             else:
                 action = [0, 1]
-        elif state_e.position[1] == 99:
-            if state_e.Qlearning[99][98] == 0 and state_e.Qlearning[99][98] == 0:
+        elif state_e.position[1] == StateD.constant.size:
+            if state_e.Qlearning[StateD.constant.size][StateD.constant.size - 1] == 0 and \
+                    state_e.Qlearning[StateD.constant.size][StateD.constant.size - 1] == 0:
                 split_1 = 0.5
             else:
-                split_1 = state_e.Qlearning[99][98] / (state_e.Qlearning[99][98] + state_e.Qlearning[98][99])
+                split_1 = state_e.Qlearning[StateD.constant.size][StateD.constant.size - 1] / (
+                            state_e.Qlearning[StateD.constant.size][StateD.constant.size - 1] +
+                            state_e.Qlearning[StateD.constant.size - 1][StateD.constant.size])
             seed = np.random.random()
             if split_1 < seed:
                 action = [-1, 0]
             else:
                 action = [0, -1]
         else:
-            if state_e.Qlearning[99][int(state_e.position[1]) + 1] == 0 and state_e.Qlearning[99][int(state_e.position[1]) - 1] == 0 and \
-                    state_e.Qlearning[98][int(state_e.position[1])] == 0:
+            if state_e.Qlearning[StateD.constant.size][int(state_e.position[1]) + 1] == 0 and \
+                    state_e.Qlearning[StateD.constant.size][int(state_e.position[1]) - 1] == 0 and \
+                    state_e.Qlearning[StateD.constant.size - 1][int(state_e.position[1])] == 0:
                 split_1 = 1 / 3
                 split_2 = 2 / 3
             else:
-                Qsum = state_e.Qlearning[99][int(state_e.position[1]) + 1] + state_e.Qlearning[99][int(state_e.position[1]) - 1] + \
-                       state_e.Qlearning[98][int(state_e.position[1])]
-                split_1 = state_e.Qlearning[99][int(state_e.position[1]) + 1] / Qsum
-                split_2 = (state_e.Qlearning[99][int(state_e.position[1]) + 1] + state_e.Qlearning[98][int(state_e.position[1])]) / Qsum
+                Qsum = state_e.Qlearning[StateD.constant.size][int(state_e.position[1]) + 1] + \
+                       state_e.Qlearning[StateD.constant.size][int(state_e.position[1]) - 1] + \
+                       state_e.Qlearning[StateD.constant.size - 1][int(state_e.position[1])]
+                split_1 = state_e.Qlearning[StateD.constant.size][int(state_e.position[1]) + 1] / Qsum
+                split_2 = (state_e.Qlearning[StateD.constant.size][int(state_e.position[1]) + 1] +
+                           state_e.Qlearning[StateD.constant.size - 1][int(state_e.position[1])]) / Qsum
             seed = np.random.random()
             if seed < split_1:
                 action = [0, 1]
@@ -75,14 +86,14 @@ def choose_action(state_e):  # 由Q表格选择策略,羊群策略记入action
                 action = [0, 1]
     else:
         if state_e.position[1] == 0:
-            Qsum = state_e.Qlearning[int(state_e.position[0])-1][0] + state_e.Qlearning[int(state_e.position[0])][1] + \
-                    state_e.Qlearning[int(state_e.position[0])+1][0]
+            Qsum = state_e.Qlearning[int(state_e.position[0]) - 1][0] + state_e.Qlearning[int(state_e.position[0])][1] + \
+                   state_e.Qlearning[int(state_e.position[0]) + 1][0]
             if Qsum == 0:
-                split_1 = 1/3
-                split_2 = 2/3
+                split_1 = 1 / 3
+                split_2 = 2 / 3
             else:
-                split_1 = state_e.Qlearning[int(state_e.position[0])-1][0]/Qsum
-                split_2 = split_1 + state_e.Qlearning[int(state_e.position[0])][1]/Qsum
+                split_1 = state_e.Qlearning[int(state_e.position[0]) - 1][0] / Qsum
+                split_2 = split_1 + state_e.Qlearning[int(state_e.position[0])][1] / Qsum
             seed = np.random.random()
             if seed < split_1:
                 action = [-1, 0]
@@ -90,15 +101,16 @@ def choose_action(state_e):  # 由Q表格选择策略,羊群策略记入action
                 action = [0, 1]
             else:
                 action = [1, 0]
-        elif state_e.position[1] == 99:
-            Qsum = state_e.Qlearning[int(state_e.position[0])-1][99] + state_e.Qlearning[int(state_e.position[0])][98] + \
-                    state_e.Qlearning[int(state_e.position[0])+1][99]
+        elif state_e.position[1] == StateD.constant.size:
+            Qsum = state_e.Qlearning[int(state_e.position[0]) - 1][StateD.constant.size] + \
+                   state_e.Qlearning[int(state_e.position[0])][StateD.constant.size - 1] + \
+                   state_e.Qlearning[int(state_e.position[0]) + 1][StateD.constant.size]
             if Qsum == 0:
-                split_1 = 1/3
-                split_2 = 2/3
+                split_1 = 1 / 3
+                split_2 = 2 / 3
             else:
-                split_1 = state_e.Qlearning[int(state_e.position[0])-1][99]/Qsum
-                split_2 = split_1 + state_e.Qlearning[int(state_e.position[0])][98]/Qsum
+                split_1 = state_e.Qlearning[int(state_e.position[0]) - 1][StateD.constant.size] / Qsum
+                split_2 = split_1 + state_e.Qlearning[int(state_e.position[0])][StateD.constant.size - 1] / Qsum
             seed = np.random.random()
             if seed < split_1:
                 action = [-1, 0]
@@ -107,10 +119,12 @@ def choose_action(state_e):  # 由Q表格选择策略,羊群策略记入action
             else:
                 action = [1, 0]
         else:
-            
+
             Qsum = state_e.Qlearning[int(state_e.position[0]) - 1][int(state_e.position[1])] + \
-                state_e.Qlearning[int(state_e.position[0])][int(state_e.position[1]) + 1] + state_e.Qlearning[int(state_e.position[0]) + 1][
-                    int(state_e.position[1])] + state_e.Qlearning[int(state_e.position[0])][int(state_e.position[1]) - 1]
+                   state_e.Qlearning[int(state_e.position[0])][int(state_e.position[1]) + 1] + \
+                   state_e.Qlearning[int(state_e.position[0]) + 1][
+                       int(state_e.position[1])] + state_e.Qlearning[int(state_e.position[0])][
+                       int(state_e.position[1]) - 1]
             if Qsum == 0:
                 split_1 = 0.25
                 split_2 = 0.5
@@ -119,7 +133,7 @@ def choose_action(state_e):  # 由Q表格选择策略,羊群策略记入action
                 split_1 = state_e.Qlearning[int(state_e.position[0]) - 1][int(state_e.position[1])] / Qsum
                 split_2 = state_e.Qlearning[int(state_e.position[0])][int(state_e.position[1]) + 1] / Qsum + split_1
                 split_3 = state_e.Qlearning[int(state_e.position[0]) + 1][int(state_e.position[1])] / Qsum + split_2
-        
+
             seed = np.random.random()
             if seed < split_1:
                 action = [-1, 0]
@@ -131,5 +145,3 @@ def choose_action(state_e):  # 由Q表格选择策略,羊群策略记入action
                 action = [0, -1]
 
     return action
-
-
